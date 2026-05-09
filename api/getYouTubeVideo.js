257b45a1880ba4acfe050291908e0eaabca12fda
Boogygigua1 <vadsbest@gmail.com>
2026-05-08 21:15:43 -0700

    Update getYouTubeVideo.js

--- api/getYouTubeVideo.js
+++ api/getYouTubeVideo.js
@@ -51,6 +51,7 @@ export default async function handler(req, res) {
 
                 const validVideo = data.items.find(item =>
                     item.id?.videoId &&
+                    !item.snippet.channelTitle.toLowerCase().includes("vevo") &&
                     !item.snippet.title.toLowerCase().includes("live") &&
                     !item.snippet.title.toLowerCase().includes("shorts") &&
                     !item.snippet.title.toLowerCase().includes("reaction") &&
